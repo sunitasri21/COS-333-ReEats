3e82ffbe2a20aecebd41ea191142d6a568bf9bac
Sunita Srivatsan <sunitasrivatsan@gmail.com>
2020-03-31 09:10:35 -0700

    vedika & arjun's updates

---
+++ db_search.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+
+#-----------------------------------------------------------------------
+# regdatabase.py
+# Author: Sunita Srivatsan
+#-----------------------------------------------------------------------
+
+from sqlite3 import connect
+from sys import stderr
+from os import path
+from menuResult import MenuResult
+
+#-----------------------------------------------------------------------
+
+class Database:
+    def __init__(self):
+        self._connection = None
+
+    def connect(self):      
+        DATABASE_NAME = 'reeats.db'
+        if not path.isfile(DATABASE_NAME):
+            raise Exception("database reeats.db not found")
+        self._connection = connect(DATABASE_NAME)
+                    
+    def disconnect(self):
+        self._connection.close()
+
+    def menuSearchRest(self, restName):
+        cursor = self._connection.cursor() 
+        restIdString = 'SELECT restaurant_id FROM restaurants ' +\
+         'WHERE restaurant_name LIKE "Chennai Chimney"'
+        cursor.execute(restIdString)
+        restId = cursor.fetchone()[0]
+        stmStr = 'SELECT food, description, unit_price, discount, food_id FROM menu ' +\
+        'WHERE menu.restaurant_id = 1;'
+        cursor.execute(stmStr)
+
+        results = []
+        row = cursor.fetchone()
+        while row is not None:  
+            result = MenuResult(str(row[0]), str(row[1]), str(row[2]), str(row[3]), str(row[4]))
+            results.append(result)
+            row = cursor.fetchone()
+        cursor.close()
+
+        return results
+
+    def menuSearchUser(self, restName):
+        cursor = self._connection.cursor() 
+        restIdString = 'SELECT restaurant_id FROM restaurants ' +\
+         'WHERE restaurant_name LIKE "Chennai Chimney"'
+        cursor.execute(restIdString)
+        restId = cursor.fetchone()[0]
+        stmStr = 'SELECT food, description, unit_price, discount, food_id FROM menu ' +\
+        'WHERE menu.restaurant_id = 1;'
+        cursor.execute(stmStr)
+
+        results = []
+        row = cursor.fetchone()
+        while row is not None:  
+            result = MenuResult(str(row[0]), str(row[1]), str(row[2]), str(row[3]), str(row[4]))
+            if float(row[3]) > 0:
+                results.append(result)
+            row = cursor.fetchone()
+        cursor.close()
+
+        return results
+
+    def updateDiscount(self, food_id, discount):
+        cursor = self._connection.cursor() 
+        restIdString = 'UPDATE menu ' +\
+        'SET discount LIKE ? ' +\
+        'WHERE food_id LIKE ?'
+        cursor.execute(restIdString)
+        cursor.close()
+
+#-----------------------------------------------------------------------
+
+# For testing:
+
+if __name__ == '__main__':
+    database = Database()
+    database.connect()
+    results = database.menuSearch("Chennai Chimney")
+    for result in results:
+        print('result', result)
+    # print(results2.getCourseId())
+    # print(results2.getArea())
+    # print(results2.getDeptandNumber())
+    # print(results2.getTitle())
+    # print(results2.getDescription())
+    # print(results2.getPrereqs())
+    # print(results2.getProfs())
+    # print(results2.getDays())
+    # print(results2.getStarttime())
+    # print(results2.getEndtime())
+    # print(results2.getBuilding())
+    # print(results2.getRoom())
+    database.disconnect()
