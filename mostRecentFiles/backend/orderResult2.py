12d2a883ea15b71807a26b073cd19db5a2ff2895
Sunita Srivatsan <sunitasrivatsan@gmail.com>
2020-04-15 18:47:44 -0700

    fully working reverse connectivity

---
+++ mostRecentFiles/backend/orderResult2.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+#-----------------------------------------------------------------------
+# OrderResult.py
+# Author: Arjun Sai Krishnan and Sunita Srivatsan 
+#-----------------------------------------------------------------------
+
+class OrderResult:
+
+    def __init__(self, food_id=1, discount='', unit_price=1, new_price=1, quantity=1, food='', description=''):
+        self.food = food
+        self.food_id = food_id 
+        self.discount = discount
+        #self.dietary = dietary
+        self.unit_price = unit_price
+        self.new_price = new_price 
+        self.quantity = quantity 
+        self.description = description
+
+    def __str__(self):
+        return self.food 
+        + ',' + self.price 
+
+    def getFood(self):
+        return self.food
+
+    def getId(self):
+        return self.food_id
+    
+    def getPrice(self):
+        return self.unit_price
+    
+    def getNewPrice(self):
+        return self.new_price
+
+    def getDescription(self):
+        return self.description
+
+    def getQuantity(self):
+        return self.quantity
+
+
+    def getDiscount(self):
+        return self.discount
+
---
+++ mostRecentFiles/backend/restdatabase2.py
@@ -0,0 +1,152 @@
+#!/usr/bin/env python
+
+#-----------------------------------------------------------------------
+# restdatabase.py
+# Author: Sunita Srivatsan and Noa Zarur
+#-----------------------------------------------------------------------
+
+from sqlite3 import connect
+from sys import stderr
+from os import path
+from orderResult2 import OrderResult
+
+# from menuResult import MenuResult
+
+#-----------------------------------------------------------------------
+
+class Database:
+    def __init__(self):
+        self._connection = None
+
+    def connect(self):      
+        DATABASE_NAME = 'reeats.db'
+        if not path.isfile(DATABASE_NAME):
+            raise Exception("database reeats.db not found")
+        self._connection = connect(DATABASE_NAME)
+                    
+    def disconnect(self):
+        self._connection.close()
+
+    def menuSearch(self, restName):
+        cursor = self._connection.cursor() 
+        restIdString = 'SELECT restaurant_id FROM restaurants ' +\
+         'WHERE restaurant_name LIKE "Chennai Chimney"'
+        cursor.execute(restIdString)
+        restId = cursor.fetchone()[0]
+        stmStr = 'SELECT food, description, unit_price, food_id FROM menu ' +\
+        'WHERE menu.restaurant_id = 1;'
+        cursor.execute(stmStr)
+
+        results = []
+        row = cursor.fetchone()
+        while row is not None:  
+            result = OrderResult(food=str(row[0]), description=str(row[1]), unit_price=str(row[2]), food_id=str(row[3]))
+            results.append(result)
+            row = cursor.fetchone()
+        cursor.close()
+
+        return results
+    
+    def menuSearchUser(self, restName):
+        cursor = self._connection.cursor() 
+        restIdString = 'SELECT restaurant_id FROM restaurants ' +\
+         'WHERE restaurant_name LIKE "Chennai Chimney"'
+        cursor.execute(restIdString)
+        restId = cursor.fetchone()[0]
+        stmStr = 'SELECT food, description, unit_price, food_id, discount, new_price FROM menu'
+        # 'WHERE menu.food_id = order_table.food_id;'
+        cursor.execute(stmStr)
+
+        results = []
+        row = cursor.fetchone()
+        while row is not None:  
+            discount = row[4]
+            if discount is not None:
+                result = OrderResult(food=str(row[0]), description=str(row[1]), unit_price=str(row[2]), food_id=str(row[3]), new_price=str(row[5]))
+                results.append(result)
+            row = cursor.fetchone()
+        cursor.close()
+
+        return results
+
+    def inputDiscount(self, discount, foodid):
+        cursor = self._connection.cursor() 
+        stmstr2 = 'SELECT food, unit_price FROM menu ' +\
+        'WHERE menu.food_id LIKE ?'
+        foodid = int(foodid)
+        cursor.execute(stmstr2, (foodid, ))
+        discount = float(discount)
+
+        result = cursor.fetchone()
+        food = result[0]
+        price = result[1]
+        quantity = 1
+        newPrice = (1 - float(discount)) * float(price)
+        newPrice = '{:.2f}'.format(newPrice)
+        stmstr = 'UPDATE menu SET discount = ?, new_price = ?, quantity = ? ' +\
+        'WHERE menu.food_id LIKE ?;'
+        arguments = (discount, newPrice, quantity, foodid) 
+        if (discount >= 0 and discount <= 1):
+            cursor.execute(stmstr, arguments) 
+        self._connection.commit()
+        cursor.close()
+        return 
+
+
+    def pullNewPrice(self, food_id):
+        cursor = self._connection.cursor() 
+        stmstr = 'SELECT new_price FROM menu ' +\
+        'WHERE menu.food_id LIKE ?;'
+        food_id = int(food_id)
+        cursor.execute(stmstr, (food_id, )) 
+        newPrice = cursor.fetchone()
+        cursor.close()
+        return newPrice[0]
+
+    def pullName(self, food_id):
+        cursor = self._connection.cursor() 
+        stmstr = 'SELECT food FROM menu ' +\
+        'WHERE menu.food_id LIKE ?;'
+        cursor.execute(stmstr, food_id)
+        newPrice = cursor.fetchone()
+        cursor.close()
+        return newPrice[0]
+
+    def previewAllDiscounts(self):
+        cursor = self._connection.cursor() 
+        stmstr = 'SELECT discount, unit_price, new_price, quantity, food FROM menu;'
+        cursor.execute(stmstr)
+        results = []
+        row = cursor.fetchone()
+        while row is not None: 
+            discount = row[0]
+            if discount is not None:
+                result = OrderResult(discount=str(row[0]), unit_price=str(row[1]), new_price=str(row[2]), quantity=str(row[3]), food=str(row[4]))
+                results.append(result)
+            row = cursor.fetchone()
+        cursor.close()
+        return results
+
+#-----------------------------------------------------------------------
+
+# For testing:
+
+if __name__ == '__main__':
+    database = Database()
+    database.connect()
+    results = database.menuSearch("Chennai Chimney")
+    for result in results:
+        print('result', result)
+    # print(results2.getCourseId())
+    # print(results2.getArea())
+    # print(results2.getDeptandNumber())
+    # print(results2.getTitle())
+    # print(results2.getDescription())
+    # print(results2.getPrereqs())
+    # print(results2.getProfs())
+    # print(results2.getDays())
+    # print(results2.getStarttime())
+    # print(results2.getEndtime())
+    # print(results2.getBuilding())
+    # print(results2.getRoom())
+    database.disconnect()
